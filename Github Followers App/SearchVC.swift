fc02e102fa1411a2b42d3d369acb4f963604cf41
Ozgundogus <ozgundogus.can@ogr.iuc.edu.tr>
2024-01-17 15:51:42 +0300

    assets update , view controllers created , tabbar created
    -Search and FavoritesLists view controller oluşturuldu .
    - App icon ve asset dosyaları oluşturuldu .
    - Tabbar oluşturuldu . Navigation Controller oluşturuldu .

---
+++ Github Followers App/SearchVC.swift
@@ -0,0 +1,22 @@
+//
+//  SearchVC.swift
+//  Github Followers App
+//
+//  Created by Ozgun Dogus on 16.01.2024.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+      
+
+    }
+    
+
+   
+
+}
